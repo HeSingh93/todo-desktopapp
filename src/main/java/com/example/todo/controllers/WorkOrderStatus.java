af09f2d4495d3893f14d850097b8755d72715f5e
niCognition <Niclas.p1990@gmail.com>
2020-09-15 12:21:41 +0200

    create workorderstatus class

---
+++ src/main/java/com/example/todo/controllers/WorkOrderStatus.java
@@ -0,0 +1,8 @@
+package com.example.todo.controllers;
+
+public class WorkOrderStatus {
+    public static final int UNASSIGNED = 1;
+    public static final int ASSIGNED = 2;
+    public static final int IN_PROGRESS = 3;
+    public static final int DONE = 4;
+}
