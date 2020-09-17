f2d0c12a5c650e40a19a9bc2e218c92b15875bbc
niCognition <Niclas.p1990@gmail.com>
2020-09-17 09:17:05 +0200

    change workorderstatus

--- src/main/java/com/example/todo/controllers/WorkOrderStatus.java
+++ src/main/java/com/example/todo/controllers/WorkOrderStatus.java
@@ -3,6 +3,6 @@ package com.example.todo.controllers;
 public class WorkOrderStatus {
     public static final int UNASSIGNED = 1;
     public static final int ASSIGNED = 2;
-    public static final int IN_PROGRESS = 3;
+    public static final int ACCEPTED = 3;
     public static final int DONE = 4;
 }
