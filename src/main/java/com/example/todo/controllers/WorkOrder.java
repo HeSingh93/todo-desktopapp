c4e72733c2da14b0b2686d50f4f1535d2d93832d
niCognition <Niclas.p1990@gmail.com>
2020-09-18 11:43:56 +0200

    create updateworkorder method
    created a method that saves a updated workorderentity-object in the
    database.

--- src/main/java/com/example/todo/controllers/WorkOrder.java
+++ src/main/java/com/example/todo/controllers/WorkOrder.java
@@ -27,8 +27,25 @@ public class WorkOrder {
         }
     }
 
-    public void updateWorkOrder() {
-        //todo: add functionality
+    public void updateWorkOrder(WorkOrderEntity workOrderEntity) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(WorkOrderEntity.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+            session.saveOrUpdate(workOrderEntity);
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            factory.close();
+            session.close();
+        }
     }
 
     public void removeWorkOrder(WorkOrderEntity workOrderEntity) {
