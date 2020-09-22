2e25203b00e544508935b8ffa1b3cfb82ea3aace
niCognition <Niclas.p1990@gmail.com>
2020-09-22 09:51:36 +0200

    add getstatus method

--- src/main/java/com/example/todo/controllers/WorkOrderStatus.java
+++ src/main/java/com/example/todo/controllers/WorkOrderStatus.java
@@ -5,4 +5,27 @@ public class WorkOrderStatus {
     public static final int ASSIGNED = 2;
     public static final int ACCEPTED = 3;
     public static final int DONE = 4;
+
+    public static String getStatus(int status) {
+        String workOrderStatus = "";
+        switch (status) {
+            case 1:
+                workOrderStatus = "Unassigned";
+                break;
+
+            case 2:
+                workOrderStatus = "Assigned";
+                break;
+
+            case 3:
+                workOrderStatus = "Accepted";
+                break;
+
+            case 4:
+                workOrderStatus = "Done";
+                break;
+        }
+
+        return workOrderStatus;
+    }
 }
